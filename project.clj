ddf071d425b309e8920b8c4e308b83aca6460a47
Morné Luus <morne@kenneth.local>
2015-02-17 14:37:37 +0800

    Updated to 1.8.1

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject link.exicon/semantic-ui "1.8.0-0"
+(defproject link.exicon/semantic-ui "1.8.1-0"
   :description  "Semantic is a UI component framework based around useful principles from natural language."
   :url          "http://semantic-ui.com"
   :license      {:name "MIT" :url "http://opensource.org/licenses/MIT"}
